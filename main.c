53cd3a63642a691ae5bd63a13b2281e3c0dcd63f
Влад Бережинский < >
2017-02-28 17:54:16 +0700

    change the main function

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
+int proffit(int x,int z);
+
 int main()
 {
-	int s,t;
-	printf("Сумма \n");
-	scanf("Сумма %f \n",s);
-	printf("Срок \n");
-	scanf("Срок %f \n",t);
-	if (s>=1000) && (t<365);
+    int s,t;
+    printf("enter time to deposit: \n");
+    scanf("%i",&t);
+    printf("enter sum to deposit: \n");
+    scanf("%i",&s);
+    if ((t <= 0) || (t > 365) || (s<10000))
+    printf("enter correct data! \n");
+    else
+	{
+		printf("sum at the end of: ");
+        printf("%i",proffit(s,t));
+    }
+    return 0;
 }
