10e301072339a62178cf62d8713a60a33aefee0c
Влад Бережинский < >
2017-02-28 18:19:12 +0700

    adding proffit function

--- main.c
+++ main.c
@@ -18,3 +18,30 @@ int main()
     }
     return 0;
 }
+
+int proffit(int x,int z){
+	int cashback=x;
+	if(x<100000)
+	{
+	    if ((z > 0) && (z <= 30)) 
+        cashback*=0.9;
+        if ((z > 30) && (z <= 120)) 
+        cashback*=1.02;
+        if ((z > 120) && (z <= 240)) 
+        cashback*=1.06;
+         if ((z > 240) && (z <= 365)) 
+        cashback*=1.12;
+    }
+	else
+	{
+		if ((z > 0) && (z <= 30)) 
+        cashback*=0.9;
+        if ((z > 30) && (z <= 120)) 
+        cashback*=1.03;
+        if ((z > 120) && (z <= 240)) 
+        cashback*=1.08;
+         if ((z > 240) && (z <= 365)) 
+        cashback*=1.15;
+	}
+return cashback;
+}
